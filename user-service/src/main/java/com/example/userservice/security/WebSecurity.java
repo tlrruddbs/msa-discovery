ab6f614e768302c733ece4f6ae59fa54f9073452
tlrruddbs <tlrruddbs@gmail.com>
2024-08-14 23:46:14 +0900

    임시저장

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,30 +1,83 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import java.util.function.Supplier;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
-@Configuration
 @EnableWebSecurity
+@Configuration
+
 public class WebSecurity {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable())  // CSRF 보호 비활성화
-        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-            .requestMatchers("/h2-console/**").permitAll()  // H2 콘솔에 대한 접근 허용
-            .requestMatchers("/user-service/**").permitAll()  // /users/** 경로에 대한 접근 허용
+  private final UserService userService;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+  private final Environment env;
 
-            .anyRequest().authenticated()  // 나머지 요청은 인증 필요
-        )
-        .headers(headers -> headers
-            .frameOptions(frameOptions -> frameOptions.disable())  // 프레임 옵션 비활성화
-        );
+  public static final String ALLOWED_IP_ADDRESS = "127.0.0.1";
+  public static final String SUBNET = "/32";
+  public static final IpAddressMatcher ALLOWED_IP_ADDRESS_MATCHER = new IpAddressMatcher(ALLOWED_IP_ADDRESS + SUBNET);
+
+  public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    this.env = env;
+    this.userService = userService;
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+  }
+
+  @Bean
+  protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+
+    AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+    http.csrf(csrf -> csrf.disable());
+    http.csrf(AbstractHttpConfigurer::disable);
+
+    http.authorizeHttpRequests((authz) -> authz
+        .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/users")).permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/users/**", HttpMethod.POST.name())).permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/welcome")).permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/health-check")).permitAll()
+        .requestMatchers("/**").access(this::hasIpAddress)
+        .anyRequest().authenticated()
+    );
+
+    http.authenticationManager(authenticationManager)
+        .sessionManagement((session) -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilter(getAuthenticationFilter(authenticationManager));
+
+    http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
 
     return http.build();
+  }
+
+  private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+    return new AuthorizationDecision(ALLOWED_IP_ADDRESS_MATCHER.matches(object.getRequest()));
+  }
+
+  private AuthenticationFilterNew getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception {
+    final CustomAuthenticationFilter filter = new CustomAuthenticationFilter(authenticationManager);
+    filter.setFilterProcessesUrl("/login");
+    return filter;
   }
 }
