2919faf2f0603d961c19f66b9f8f2a5077ee0488
tlrruddbs <tlrruddbs@gmail.com>
2024-09-11 12:34:40 +0900

    token값 변경

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -1,7 +1,9 @@
 package com.example.gatewayservice;
 
+import org.apache.http.client.methods.HttpTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -9,5 +11,4 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
 }
