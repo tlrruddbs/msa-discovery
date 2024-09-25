55a5cd2000a61830b0251a0dc5dfcf1961933981
tlrruddbs <tlrruddbs@naver.com>
2024-09-25 14:44:36 +0900

    user-service circuitbreaker 도입

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -2,26 +2,23 @@ package com.example.userservice.service;
 
 import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
-import com.example.userservice.error.FeignErrorDecoder;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
-import feign.FeignException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 import org.springframework.util.ObjectUtils;
 
 @Service
@@ -34,6 +31,9 @@ public class UserServiceImpl implements UserService{
     private final BCryptPasswordEncoder passwordEncoder;
 
     private final OrderServiceClient orderServiceClient;
+
+    private final CircuitBreakerFactory circuitBreakerFactory;
+
 
     @Override
     public UserDto createUser(final UserDto userDto) {
@@ -68,8 +68,11 @@ public class UserServiceImpl implements UserService{
 //        } catch (FeignException ex){
 //            log.error(ex.getMessage());
 //        }
-        List<ResponseOrder> ordersList = orderServiceClient.getOrders(userId);
-
+//        List<ResponseOrder> ordersList = orderServiceClient.getOrders(userId);
+        CircuitBreaker circuitbreaker = circuitBreakerFactory.create("circuitbreaker");
+        List<ResponseOrder> ordersList = circuitbreaker.run(() -> orderServiceClient.getOrders(userId),
+            throwable -> new ArrayList<>()
+        );
 //        List<ResponseOrder> orders = new ArrayList<>();
         userDto.setOrders(ordersList);
 
